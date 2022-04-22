2ff41d0f39c4af51c6e07d4450c5db78df5b236b
Artem Veremienko <temaspsxxx@gmail.com>
2022-04-23 00:16:33 +0300

    fix: update color

--- src/StyledButton/StyledButton.tsx
+++ src/StyledButton/StyledButton.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Button } from '@mui/material';
 import { styled } from '@mui/material/styles';
+import { grey } from '@mui/material/colors';
 
 export const StyledButton = styled(Button)`
   color: ${({ theme }) => {
     // console.log(theme);
-    return theme.palette.grey[50];
+    return theme.palette.grey[800];
   }};
 `;
