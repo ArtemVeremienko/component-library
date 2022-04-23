ec37cdb6e7bedc14f9f80ee8902d338b0638ab24
Artem Veremienko <temaspsxxx@gmail.com>
2022-04-23 14:51:56 +0300

    feat: update styles

--- src/CustomCheckbox/CustomCheckbox.tsx
+++ src/CustomCheckbox/CustomCheckbox.tsx
@@ -3,6 +3,6 @@ import { Checkbox, styled } from '@mui/material';
 export const CustomCheckbox = styled(Checkbox)(({ theme }) => ({
   color: theme.status.danger,
   '&.Mui-checked': {
-    color: theme.status.danger,
+    color: theme.palette.error.dark,
   },
 }));
--- src/StyledButton/StyledButton.tsx
+++ src/StyledButton/StyledButton.tsx
@@ -2,7 +2,6 @@ import { Button, styled } from '@mui/material';
 
 export const StyledButton = styled(Button)`
   color: ${({ theme }) => {
-    // console.log(theme);
-    return theme.palette.grey[800];
+    return theme.palette.grey[900];
   }};
 `;
